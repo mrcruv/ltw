f96e1d7b095b36d3dee4a1005bcee65df7e1b288
deto235 <102602333+deto235@users.noreply.github.com>
2022-04-14 08:59:04 +0200

    Update footer.php
    Aggiungo codice del footer con dati presi dal file info.php.

--- includes/footer.php
+++ includes/footer.php
@@ -1,1 +1,10 @@
 <?php
+    require_once 'info.php';
+    global $sitename, $current_year, $authors;
+?>
+
+<footer class="text-center mt-auto" id="footer">
+    <div>
+        <?php echo"<p>$sitename © $current_year Authors: ".implode(",",$authors);"</p>"?>
+    </div>
+</footer>
