b61d0f4a768dd6da510a9077f37a14975d9e8bd2
deto235 <102602333+deto235@users.noreply.github.com>
2022-04-14 09:33:30 +0200

    Update header.php
    Aggiunto codice dell'header. Navbar con diverse opzioni

--- includes/header.php
+++ includes/header.php
@@ -1,1 +1,29 @@
-<?php
+<nav class="navbar navbar-expand-lg navbar-light" id="nav_header">
+        <div class="container-fluid">
+          <div class="title">
+            <a class="navbar-brand">DigitalOutsourching</a>
+          </div>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="">Opzione 1</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="">Opzione 2</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="">Opzione 3</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="">Opzione 4</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="">Opzione 5</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+    </nav>
