42da934010d915bd67828b8b409abce8a26e2905
deto235 <102602333+deto235@users.noreply.github.com>
2022-04-15 12:07:24 +0200

    Update login.php
    Aggiunto script al file login.php che apre la sessione se il login è corretto.

--- scripts/login.php
+++ scripts/login.php
@@ -1,1 +1,33 @@
 <?php
+require_once("../includes/open_connection.php");
+global $connection;
+
+if (!isset($_POST["login_submit"])) {
+    header ("Location: ../index.php");
+}
+
+$username = $_POST["username"];
+$password = $_POST["password"];
+$hash = password_hash($password, PASSWORD_BCRYPT);
+
+
+
+$query = "SELECT * FROM utenti WHERE username=? OR password=?";
+$statement = mysqli_prepare($connection, $query) or die(mysqli_error($connection));
+mysqli_stmt_bind_param($statement, 'ss', $username, $hash) or die(mysqli_error($connection));
+mysqli_stmt_execute($statement) or die(mysqli_error($connection));
+
+if (!mysqli_stmt_fetch($statement)) {
+    header("Location: ../index.php");
+}
+else{
+    //si avvia la sessione
+    session_start();
+
+    $_SESSION['username'] = $username;
+    header("Location: ../me.php");
+
+
+}
+
+require_once("../includes/close_connection.php");
