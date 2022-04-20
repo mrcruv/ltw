50534301c11053a62e4fb5740aee8a38b6349f89
Marco Ruvolo <marco.ruvolo@protonmail.com>
2022-04-20 17:14:47 +0200

    Create form_switch.js
    aggiunto script per lo switch del form di registrazione utente ente/utente espero

---
+++ scripts/form_switch.js
@@ -0,0 +1,14 @@
+function switch_form_handler() {
+    $("#expert_button").click(function() {
+        $("#expert_fields").show();
+        $("#entity_fields").hide();
+        $(this).attr("disabled", true)
+        $("#entity_button").attr("disabled", false)
+    })
+    $("#entity_button").click(function(){
+        $("#expert_fields").hide();
+        $("#entity_fields").show();
+        $(this).attr("disabled", true)
+        $("#expert_button").attr("disabled", false)
+    })
+}
