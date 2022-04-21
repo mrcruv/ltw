409811762e43b40f7b6e6aafed193b370643de54
Marco Ruvolo <marco.ruvolo@protonmail.com>
2022-04-21 10:22:35 +0200

    Update login.php
    fix login

--- scripts/login.php
+++ scripts/login.php
@@ -12,22 +12,22 @@ $hash = password_hash($password, PASSWORD_BCRYPT);
 
 
 
-$query = "SELECT * FROM utenti WHERE username=? OR password=?";
+$query = "SELECT password FROM utenti WHERE username=?";
 $statement = mysqli_prepare($connection, $query) or die(mysqli_error($connection));
-mysqli_stmt_bind_param($statement, 'ss', $username, $hash) or die(mysqli_error($connection));
+mysqli_stmt_bind_param($statement, 's', $username) or die(mysqli_error($connection));
 mysqli_stmt_execute($statement) or die(mysqli_error($connection));
+mysqli_stmt_bind_result($statement, $result_password);
 
 if (!mysqli_stmt_fetch($statement)) {
     header("Location: ../index.php");
 }
-else{
-    //si avvia la sessione
+else if (password_verify($password, $result_password)){
     session_start();
-
     $_SESSION['username'] = $username;
     header("Location: ../me.php");
-
-
+}
+else {
+    header("Location: ../index.php");
 }
 
 require_once("../includes/close_connection.php");
