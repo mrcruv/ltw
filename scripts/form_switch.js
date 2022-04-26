6ee96a0ed1bdd53bf1c5771aef81fa710df4358d
deto235 <102602333+deto235@users.noreply.github.com>
2022-04-26 14:23:20 +0200

    Update form_switch.js
    Aggiunto switch della checkbox

--- scripts/form_switch.js
+++ scripts/form_switch.js
@@ -4,11 +4,13 @@ function switch_form_handler() {
         $("#entity_fields").hide();
         $(this).attr("disabled", true)
         $("#entity_button").attr("disabled", false)
+        $("#register_user_type").prop('checked', false);
     })
     $("#entity_button").click(function(){
         $("#expert_fields").hide();
         $("#entity_fields").show();
         $(this).attr("disabled", true)
         $("#expert_button").attr("disabled", false)
+        $("#register_user_type").prop('checked', true);
     })
 }
