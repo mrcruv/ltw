d80aebdbe1211f8b0d25aa4ccc787c87a79d4b64
Marco Ruvolo <marco.ruvolo@protonmail.com>
2022-04-26 21:11:39 +0200

    Update assegnazioni.php
    aggiornamento visualizzazione in base al tipo di utente e modifica form inserimento

--- assegnazioni.php
+++ assegnazioni.php
@@ -1,3 +1,12 @@
+<?php
+if(!isset($_SESSION))
+{
+    session_start();
+}
+$username = $_SESSION["username"];
+$usertype = $_SESSION["usertype"];
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -19,6 +28,7 @@
 
 <?php require_once 'scripts/session.php'; ?>
 
+<?php if ($usertype == 'ente') { ?>
 <form id="add_availability_form" action="scripts/add_availability.php" method="post">
     <div>
     <label for="process">Processo</label>
@@ -31,7 +41,7 @@
             $n = count($array);
             for ($i = 0; $i < $n; $i += 1) {
                 echo("<option value='$array[$i]['name']'>");
-                echo("<td>" . $array[$i]["name"] . "</td>");
+                echo($array[$i]["name"]);
                 echo("</option>");
             }
             ?>
@@ -41,12 +51,75 @@
     </div>
     <div>
     <label for="expert">Esperto</label>
-    <input type="text" id="expert" placeholder="Inserisci esperto" name="expert">
+        <div>
+            <select id="availability_expert" name="expert">
+                <option selected>Scegli l'esperto</option>
+                <?php
+                require_once 'scripts/show_expert.php';
+                $array = show_all_experts();
+                $n = count($array);
+                for ($i = 0; $i < $n; $i += 1) {
+                    echo("<option value='$array[$i]['username']'>");
+                    echo($array[$i]["username"]);
+                    echo("</option>");
+                }
+                ?>
+            </select>
+        </div>
     </div>
     <button type="submit" name="add_availability_submit">Aggiungi assegnazione</button>
 </form>
 
-<?php //require_once 'scripts/show_availability.php'; ?>
+    <?php
+    require_once 'scripts/show_availability.php';
+    echo("<table>");
+    echo("<tr><th>PROCESSO</th><th>ESPERTO</th><th>DATA RICHIESTA</th>
+    <th>DATA ASSEGNAZIONE</th><th>DATA RIFIUTO</th></tr>");
+    $array = show_all_availabilities_entity();
+    $n = count($array);
+    if (!is_array($array) or $n <= 0) {
+        echo('<tr><td colspan="5">NON CI SONO ASSEGNAZIONI AL MOMENTO</td></tr>');
+    }
+    else {
+        for ($i = 0; $i < $n; $i += 1) {
+            echo("<tr>");
+            echo("<td>" . $array[$i]["process"] . "</td>");
+            echo("<td>" . $array[$i]["expert"] . "</td>");
+            echo("<td>" . $array[$i]["request_date"] . "</td>");
+            echo("<td>" . $array[$i]["allocation_date"] . "</td>");
+            echo("<td>" . $array[$i]["rejection_date"] . "</td>");
+            echo("</tr>");
+        }
+    }
+    echo("</table>");
+    ?>
+
+<?php } else { ?>
+
+<?php
+require_once 'scripts/show_availability.php';
+echo("<table>");
+echo("<tr><th>PROCESSO</th><th>ENTE</th><th>DATA RICHIESTA</th>
+    <th>DATA ASSEGNAZIONE</th><th>DATA RIFIUTO</th></tr>");
+$array = show_all_availabilities_expert();
+$n = count($array);
+if (!is_array($array) or !isset($array['process'])) {
+    echo('<tr><td colspan="5">NON CI SONO ASSEGNAZIONI AL MOMENTO</td></tr>');
+}
+else {
+    for ($i = 0; $i < $n; $i += 1) {
+        echo("<tr>");
+        echo("<td>" . $array[$i]["process"] . "</td>");
+        echo("<td>" . $array[$i]["entity"] . "</td>");
+        echo("<td>" . $array[$i]["request_date"] . "</td>");
+        echo("<td>" . $array[$i]["allocation_date"] . "</td>");
+        echo("<td>" . $array[$i]["rejection_date"] . "</td>");
+        echo("</tr>");
+    }
+}
+echo("</table>");
+?>
+<?php } ?>
 
 <?php require_once 'includes/footer.php'; ?>
 </body>
