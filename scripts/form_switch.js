ca5a96a9a434c4f9fad2a2408f4a739475c1324b
Marco Ruvolo <marco.ruvolo@protonmail.com>
2022-04-26 19:16:58 +0200

    Merge form script
    unione versioni script per lo switch del form di registrazione

--- scripts/form_switch.js
+++ scripts/form_switch.js
@@ -1,16 +1,38 @@
-function switch_form_handler() {
+$(document).ready(function(){
+    $("#expert_fields").hide();
+    $("#text_expert").hide();
+    $("#entity_button").attr("disabled", true)
+    $("#expert_button").attr("disabled", false)
+    $("#register_user_type").prop('checked', true);
+
     $("#expert_button").click(function() {
         $("#expert_fields").show();
         $("#entity_fields").hide();
         $(this).attr("disabled", true)
         $("#entity_button").attr("disabled", false)
+        $("#expert_button").attr("disabled", true)
+        $("#text_expert").show();
+        $("#text_entity").hide();
         $("#register_user_type").prop('checked', false);
-    })
+    });
+
     $("#entity_button").click(function(){
         $("#expert_fields").hide();
         $("#entity_fields").show();
         $(this).attr("disabled", true)
         $("#expert_button").attr("disabled", false)
+        $("#entity_button").attr("disabled", true)
+        $("#text_entity").show();
+        $("#text_expert").hide();
         $("#register_user_type").prop('checked', true);
-    })
-}
+    });
+
+    $('.nav-item a').on('click', function(e) {
+        e.preventDefault();
+        $('.nav-item a').removeClass('active');
+        $(this).addClass('active');
+        $('.tab').removeClass('active');
+        var tabID = $(this).attr('data-target');
+        $('.tab[data-tab="' + tabID + '"]').addClass('active');
+    });
+});
