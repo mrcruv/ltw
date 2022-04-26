9f8e720bc4d5aa8e2ce3a434497da8a7e27a3e35
deto235 <102602333+deto235@users.noreply.github.com>
2022-04-26 17:42:48 +0200

    Update header.php
    Aggiunta divisione della navbar tra ente e esperto

--- includes/header.php
+++ includes/header.php
@@ -1,6 +1,14 @@
 <?php
 require_once("info.php");
 global $sitename;
+if(!isset($_SESSION))
+{
+    session_start();
+}
+
+$username = $_SESSION["username"];
+$usertype = $_SESSION["usertype"];
+
 ?>
 
 <nav class="navbar navbar-expand-lg navbar-light" id="nav_header">
@@ -13,27 +21,32 @@ global $sitename;
           </button>
           <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="me.php">Dashboard</a>
-                </li>
+              <li class="nav-item">
+                <a class="nav-link" href="me.php">Dashboard</a>
+              </li>
+          <?php if($usertype == 'ente') { ?>
               <li class="nav-item">
                 <a class="nav-link" href="processi.php">Processi</a>
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="esperti.php">Esperti</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="assegnazioni.php">Assegnazioni</a>
-              </li>
+          <?php }
+                else{
+          ?>
               <li class="nav-item">
                 <a class="nav-link" href="titoli.php">Titoli di studio</a>
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="competenze.php">Competenze</a>
               </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="scripts/logout.php"><button type="button">LOGOUT</button></a>
-                </li>
+          <?php } ?>
+              <li class="nav-item">
+                <a class="nav-link" href="assegnazioni.php">Assegnazioni</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="scripts/logout.php"><button type="button">LOGOUT</button></a>
+              </li>
             </ul>
           </div>
         </div>
