f2844d72fbecbaedbdc19db95b5ebbd820028844
Marco Ruvolo <marco.ruvolo@protonmail.com>
2022-04-26 07:32:44 +0200

    Update me.php
    fix minori

--- me.php
+++ me.php
@@ -1,3 +1,13 @@
+<?php
+require_once 'includes/info.php';
+global $sitename;
+if(!isset($_SESSION))
+{
+    session_start();
+}
+$username = $_SESSION["username"];
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,7 +16,7 @@
     <meta name="description" content=""/>
     <meta name="author" content=""/>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Title</title>
+    <title><?php echo($sitename . " - " . $username); ?></title>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
@@ -18,6 +28,8 @@
     <?php require_once 'includes/header.php'; ?>
 
     <?php require_once 'scripts/session.php'; ?>
+
+
 
     <?php require_once 'includes/footer.php'; ?>
 
