2ecea0bb2169e28ef69fc3cac018803f0cccda0d
deto235 <102602333+deto235@users.noreply.github.com>
2022-04-26 14:25:17 +0200

    Update register.php
    Aggiunto divisione della registrazione tra ente e esperto tramite la checkbox nel form

--- scripts/register.php
+++ scripts/register.php
@@ -11,18 +11,6 @@ $password = $_POST["password"];
 $cf = $_POST["cf"];
 $pec = $_POST["pec"];
 $piva = $_POST["piva"];
-$website = $_POST["website"];
-$accept_conditions = $_POST["accept_conditions"];
-
-$company_name = $_POST["entity_name"];
-$type = $_POST["type"];
-
-//$name = $_POST["name"];
-//$surname = $_POST["surname"];
-//$city = $_POST["city"];
-//$date = $_POST["city"];
-
-$hash = password_hash($password, PASSWORD_BCRYPT);
 
 $query = "SELECT * FROM utenti WHERE username=? OR pec=? OR cf=? OR piva=?";
 $statement = mysqli_prepare($connection, $query) or die(mysqli_error($connection));
@@ -33,23 +21,42 @@ if (mysqli_stmt_fetch($statement)) {
     header("Location: ../index.php");
 }
 
+$website = $_POST["website"];
+$accept_conditions = $_POST["accept_conditions"];
+
+$hash = password_hash($password, PASSWORD_BCRYPT);
+
 $query = "INSERT INTO utenti(username, password, pec, cf, piva, sito_web)
-                    VALUES (?, ?, ?, ?, ?, ?);";
+                VALUES (?, ?, ?, ?, ?, ?)";
 $statement = mysqli_prepare($connection, $query) or die(mysqli_error($connection));
 mysqli_stmt_bind_param($statement, 'ssssss', $username, $hash, $pec, $cf, $piva, $website) or die(mysqli_error($connection));
 mysqli_stmt_execute($statement) or die(mysqli_error($connection));
 
-$query = "INSERT INTO enti(username, denominazione, tipo)
-                    VALUES (?, ?, ?);";
-$statement = mysqli_prepare($connection, $query) or die(mysqli_error($connection));
-mysqli_stmt_bind_param($statement, 'sss', $username, $company_name, $type) or die(mysqli_error($connection));
-mysqli_stmt_execute($statement) or die(mysqli_error($connection));
+if(isset($_POST['user_type'])){
+    
+    $company_name = $_POST["entity_name"];
+    $type = $_POST["type"];
 
-//$query = "INSERT INTO esperti(username, nome, cognome, citta, data)
-//                    VALUES (?, ?, ?, ?, ?);";
-//$statement = mysqli_prepare($connection, $query) or die(mysqli_error($connection));
-//mysqli_stmt_bind_param($statement, 'sssss', $username, $name, $surname, $city, $date) or die(mysqli_error($connection));
-//mysqli_stmt_execute($statement) or die(mysqli_error($connection));
+    $query = "INSERT INTO enti(username, denominazione, tipo)
+                    VALUES (?, ?, ?)";
+    $statement = mysqli_prepare($connection, $query) or die(mysqli_error($connection));
+    mysqli_stmt_bind_param($statement, 'sss', $username, $company_name, $type) or die(mysqli_error($connection));
+    mysqli_stmt_execute($statement) or die(mysqli_error($connection));
+}
+else if(!isset($_POST['user_type'])){
+    
+    $name = $_POST["name"];
+    $surname = $_POST["surname"];
+    $city = $_POST["city"];
+    $date = $_POST["date"];
+
+    $query = "INSERT INTO esperti(username, nome, cognome, citta_nascita, data_nascita)
+                    VALUES (?, ?, ?, ?, ?)";
+    $statement = mysqli_prepare($connection, $query) or die(mysqli_error($connection));
+    mysqli_stmt_bind_param($statement, 'sssss', $username, $name, $surname, $city, $date) or die(mysqli_error($connection));
+    mysqli_stmt_execute($statement) or die(mysqli_error($connection));
+}
+
 
 require_once("../includes/close_connection.php");
 
