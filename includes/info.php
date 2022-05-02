49c0f6b10184d32ed8f3fa75efeb4871f5a84a72
Marco Ruvolo <marco.ruvolo@protonmail.com>
2022-05-02 11:45:53 +0200

    Update info.php
    aggiunta path

--- includes/info.php
+++ includes/info.php
@@ -2,3 +2,41 @@
 $sitename = "Digital Outsourcing";
 $current_year = "2022";
 $authors = ["Federico Detomaso", "Marco Ruvolo"];
+$root = "C:/xampp/htdocs/ltw/";
+$paths = Array(
+    "includes" => $root . "includes/",
+    "info" => $root . "includes/info.php",
+    "header" => $root . "includes/header.php",
+    "footer" => $root . "includes/footer.php",
+    "open_connection" => $root . "includes/open_connection.php",
+    "close_connection" => $root . "includes/close_connection.php",
+    "config" => $root . "includes/config.php",
+
+    "scripts" => $root . "scripts/",
+    "accept_reject" => $root . "scripts/accept_reject.php",
+    "add_availability" => "scripts/add_availability.php",
+    "add_competence" => "scripts/add_competence.php",
+    "add_process" => "scripts/add_process.php",
+    "add_title" => "scripts/add_title.php",
+    "show_availability" => $root . "scripts/show_availability.php",
+    "show_competence" => $root . "scripts/show_competence.php",
+    "show_expert" => $root . "scripts/show_expert.php",
+    "show_process" => $root . "scripts/show_process.php",
+    "show_title" => $root . "scripts/show_title.php",
+    "login" => "scripts/login.php",
+    "logout" => $root . "scripts/logout.php",
+    "register" => "scripts/register.php",
+    "session" => $root . "scripts/session.php",
+    "update_password" => "scripts/update_password.php",
+    "form_switch" => $root . "scripts/form_switch.js",
+    "validate_login" => $root . "scripts/validate_login.js",
+    "validate_register" => $root . "scripts/validate_register.js",
+
+    "assegnazioni" => $root . "assegnazioni.php",
+    "competenze" => $root . "competenze.php",
+    "esperti" => $root . "esperti.php",
+    "index" => $root . "index.php",
+    "me" => $root . "me.php",
+    "processi" => $root . "processi.php",
+    "titoli" => $root . "titoli.php"
+);
