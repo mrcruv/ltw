32bfe5212e144f2920fe6ba0b1c72d33365a4d77
deto235 <102602333+deto235@users.noreply.github.com>
2022-05-04 10:48:08 +0200

    Update and rename register.php to register_expert.php
    Modifica registrazione per esperti

--- scripts/register_expert.php
+++ scripts/register_expert.php
@@ -5,11 +5,12 @@ if (!isset($_POST["update_password_submit"])) {
     header ("Location: ../me.php");
 }
 
-$username = $_POST["username"];
-$password = $_POST["password"];
-$cf = $_POST["cf"];
-$pec = $_POST["pec"];
-$piva = $_POST["piva"];
+
+$username = $_POST["expert_username"];
+$password = $_POST["expert_password"];
+$cf = $_POST["expert_cf"];
+$pec = $_POST["expert_pec"];
+$piva = $_POST["expert_piva"];
 
 $query = "SELECT * FROM utenti WHERE username=? OR pec=? OR cf=? OR piva=?";
 $statement = mysqli_prepare($connection, $query) or die(mysqli_error($connection));
@@ -21,29 +22,17 @@ if (mysqli_stmt_fetch($statement)) {
 }
 mysqli_stmt_close($statement) or die(mysqli_error($connection));
 
-$website = $_POST["website"];
-$accept_conditions = $_POST["accept_conditions"];
+$website = $_POST["expert_website"];
+#$accept_conditions = $_POST["accept_conditions"];
 $hash = password_hash($password, PASSWORD_BCRYPT);
 
-$query = "INSERT INTO utenti(username, password, pec, cf, piva, sito_web)
-                VALUES (?, ?, ?, ?, ?, ?)";
-$statement = mysqli_prepare($connection, $query) or die(mysqli_error($connection));
-mysqli_stmt_bind_param($statement, 'ssssss', $username, $hash, $pec, $cf, $piva, $website) or die(mysqli_error($connection));
-mysqli_stmt_execute($statement) or die(mysqli_error($connection));
-mysqli_stmt_close($statement) or die(mysqli_error($connection));
-
-if(isset($_POST['register_box'])){
-    $company_name = $_POST["entity_name"];
-    $type = $_POST["type"];
-
-    $query = "INSERT INTO enti(username, denominazione, tipo)
-                    VALUES (?, ?, ?)";
+    $query = "INSERT INTO utenti(username, password, pec, cf, piva, sito_web)
+                    VALUES (?, ?, ?, ?, ?, ?)";
     $statement = mysqli_prepare($connection, $query) or die(mysqli_error($connection));
-    mysqli_stmt_bind_param($statement, 'sss', $username, $company_name, $type) or die(mysqli_error($connection));
+    mysqli_stmt_bind_param($statement, 'ssssss', $username, $hash, $pec, $cf, $piva, $website) or die(mysqli_error($connection));
     mysqli_stmt_execute($statement) or die(mysqli_error($connection));
     mysqli_stmt_close($statement) or die(mysqli_error($connection));
-}
-else {
+
     $name = $_POST["name"];
     $surname = $_POST["surname"];
     $city = $_POST["city"];
@@ -55,7 +44,7 @@ else {
     mysqli_stmt_bind_param($statement, 'sssss', $username, $name, $surname, $city, $date) or die(mysqli_error($connection));
     mysqli_stmt_execute($statement) or die(mysqli_error($connection));
     mysqli_stmt_close($statement) or die(mysqli_error($connection));
-}
 
-//require_once("../includes/close_connection.php");
-header("Location: ../index.php");
+
+    //require_once("../includes/close_connection.php");
+    header("Location: ../index.php");
