fbafb833490ce5617263f1d38336bef6df354f0f
deto235 <102602333+deto235@users.noreply.github.com>
2022-05-04 10:46:27 +0200

    Update form_switch.js
    Modifica per i due form distinti

--- scripts/form_switch.js
+++ scripts/form_switch.js
@@ -1,30 +1,25 @@
 $(document).ready(function(){
-    $("#expert_fields").hide();
+    $("#register_expert_form").hide();
     $("#text_expert").hide();
-    $("#div_user_type").hide();
     $("#entity_button").attr("disabled", true)
     $("#expert_button").attr("disabled", false)
 
     $("#expert_button").click(function() {
-        $("#expert_fields").show();
-        $("#entity_fields").hide();
+        $("#register_expert_form").show();
+        $("#register_entity_form").hide();
         $(this).attr("disabled", true)
         $("#entity_button").attr("disabled", false)
-        $("#expert_button").attr("disabled", true)
         $("#text_expert").show();
         $("#text_entity").hide();
-        $("#usertype_box").prop('checked', false);
     });
 
     $("#entity_button").click(function(){
-        $("#expert_fields").hide();
-        $("#entity_fields").show();
+        $("#register_expert_form").hide();
+        $("#register_entity_form").show();
         $(this).attr("disabled", true)
         $("#expert_button").attr("disabled", false)
-        $("#entity_button").attr("disabled", true)
         $("#text_entity").show();
         $("#text_expert").hide();
-        $("#usertype_box").prop('checked', true);
     });
 
     $('.nav-item a').on('click', function(e) {
