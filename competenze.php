164fb420afa109705ea64c6d1752b5610ef1918c
deto235 <102602333+deto235@users.noreply.github.com>
2022-05-08 12:10:46 +0200

    Update competenze.php
    aggiunta grafica

--- competenze.php
+++ competenze.php
@@ -25,45 +25,65 @@ $usertype = $_SESSION['usertype'];
 </head>
 <body class="d-flex flex-column min-vh-100">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="scripts/add_form.js"></script>
+    
+    <?php require_once('includes/header.php');
 
-    <?php require_once('includes/header.php'); ?>
-
-    <form id="add_competence_form" action="<?php echo('scripts/add_competence.php'); ?>" method="post">
-        <div>
-            <label for="competence_name">Nome competenza</label>
-            <input type="text" id="competence_name" placeholder="Inserisci nome" name="name">
-        </div>
-        <div>
-            <label for="competence_area">Settore competenza</label>
-            <input type="text" id="competence_area" placeholder="Inserisci settore" name="area">
-        </div>
-        <div>
-            <label for="competence_description">Descrizione</label>
-            <input type="text" id="competence_description" placeholder="Inserisci descrizione" name="description">
-        </div>
-        <button type="submit" name="add_competence_submit">Aggiungi competenza</button>
-    </form>
-
-    <?php
     require_once('scripts/show_competence.php');
-    echo('<table>');
-    echo('<tr><th>COMPETENZA</th><th>SETTORE</th><th>DESCRIZIONE</th></tr>');
-    $array = show_all_competences($_SESSION['username']);
-    $n = count($array);
-    if (!is_array($array) or $n <= 0) {
-        echo('<tr><td colspan="3">NON CI SONO COMPETENZE AL MOMENTO</td></tr>');
-    }
-    else {
-        for ($i = 0; $i < $n; $i += 1) {
-            echo('<tr>');
-            echo('<td>' . $array[$i]['name'] . '</td>');
-            echo('<td>' . $array[$i]['area'] . '</td>');
-            echo('<td>' . $array[$i]['description'] . '</td>');
-            echo('</tr>');
-        }
-    }
-    echo('</table>');
     ?>
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-3 offset-md-4 text-center align-middle">
+                <h5>Aggiungi competenza</h5>
+                <a class="btn btn-primary rounded-circle" id="add_button">+</a>
+
+                <form id="add_competence_form" class="add_form" action="<?php echo('scripts/add_competence.php'); ?>" method="post">
+                    <div class="form-group mb-3 mt-4">
+                        <input type="text" id="competence_name" class="form-control" placeholder="Nome competenza" name="name">
+                    </div>
+                    <div class="form-group mb-3">
+                        <input type="text" id="competence_area" class="form-control" placeholder="Settore" name="area">
+                    </div>
+                    <div class="form-group mb-3">
+                        <textarea class="form-control" id="competence_description" placeholder="Descrizione" name="description" rows="3"></textarea>
+                    </div class="form-group mb-3">
+                    <button type="submit" class="btn btn-primary" name="add_competence_submit">Aggiungi</button>
+                </form>
+            </div>
+        </div>
+
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Competenza</th>
+                    <th scope="col">Settore</th>
+                    <th scope="col">Descrizione</th>
+                </tr>
+            </thead>
+            <tbody>
+            <?php $array = show_all_competences($_SESSION['username']);
+            $n = count($array);
+            if (!is_array($array) or $n <= 0) { ?>
+                <tr><td colspan="4"><h6>Non ci sono Competenze al momento</h6></td></tr>
+            <?php
+            }
+            else {
+                for ($i = 0; $i < $n; $i += 1) { ?>
+                <tr>
+                    <th scope="row"><?php echo $i+1?></th>
+                    <td><?php echo $array[$i]['name']?></td>
+                    <td><?php echo $array[$i]['area']?></td>
+                    <td><?php echo $array[$i]['description']?></td>
+                </tr>
+                <?php
+                }
+            }  ?> 
+            </tbody>
+        </table>
+
+    </div>
 
     <?php require_once('includes/footer.php'); ?>
 </body>
