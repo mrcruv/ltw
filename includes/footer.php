e5ab8a3ea04dc47995b190345a509c7e53148ade
Marco Ruvolo <marco.ruvolo@protonmail.com>
2022-05-08 16:26:28 +0200

    Update footer.php
    fix minori

--- includes/footer.php
+++ includes/footer.php
@@ -1,10 +1,10 @@
 <?php
-    require_once 'info.php';
+    require_once('info.php');
     global $sitename, $current_year, $authors;
 ?>
 
 <footer class="text-center mt-auto" id="footer">
     <div>
-        <?php echo('<p>$sitename © $current_year - Authors: ' . implode(', ',$authors) . '</p>'); ?>
+        <?php echo('<p>' . $sitename . ' © ' . $current_year . ' - Authors: ' . implode(', ',$authors) . '</p>'); ?>
     </div>
 </footer>
