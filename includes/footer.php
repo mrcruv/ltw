ce0b767d39908819e9a4b7648d7d2722d7db020f
deto235 <102602333+deto235@users.noreply.github.com>
2022-05-08 18:30:40 +0200

    Update footer.php
    fix grafica

--- includes/footer.php
+++ includes/footer.php
@@ -3,8 +3,8 @@
     global $sitename, $current_year, $authors;
 ?>
 
-<footer class="text-center mt-auto" id="footer">
+<footer class="text-center mt-auto shadow p-3 rounded" id="footer">
     <div>
-        <?php echo('<p>' . $sitename . ' © ' . $current_year . ' - Authors: ' . implode(', ',$authors) . '</p>'); ?>
+        <span class="align-middle h6"><?php echo $sitename . ' © ' . $current_year . ' - Authors: ' . implode(', ',$authors) ?></span>
     </div>
 </footer>
