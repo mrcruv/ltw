9d4cbf1e4cb741ec3892cfd18f90531c29591403
Marco Ruvolo <marco.ruvolo@protonmail.com>
2022-05-10 21:23:08 +0200

    Create message.php
    creazione modulo alert messaggi

---
+++ includes/message.php
@@ -0,0 +1,16 @@
+<div class="modal fade" id="messageModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">MESSAGGIO</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="CHIUDI"></button>
+            </div>
+            <div class="modal-body">
+                <p><?php echo strtoupper($_GET['msg']); ?></p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">CHIUDI</button>
+            </div>
+        </div>
+    </div>
+</div>
