7a4880d90c4bd58ee74be67b4143e00fe96ed7c1
deto235 <102602333+deto235@users.noreply.github.com>
2022-05-10 22:35:46 +0200

    Update index.php
    Aggiunto toggle della password

--- index.php
+++ index.php
@@ -18,6 +18,7 @@ if (isset($_SESSION['username'])) {
     <meta name="author" content=""/>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
 
@@ -75,6 +76,7 @@ if (isset($_SESSION['username'])) {
 
                                 <div class="form-outline mb-4">
                                     <input type="password" id="login_password" class="form-control" placeholder="Password" name="password" aria-describedby="password_help"/>
+                                    <span toggle="#login_password" class="fa fa-fw fa-eye field-icon toggle-password"></span>
                                 </div>
 
                                 <div class="row mb-4">
@@ -129,6 +131,7 @@ if (isset($_SESSION['username'])) {
 
                                 <div class="form-outline mb-4">
                                     <input type="password" aria-describedby="password_help" id="register_entity_password" class="form-control" placeholder="Password" name="entity_password"/>
+                                    <span toggle="#register_entity_password" class="fa fa-fw fa-eye field-icon toggle-password"></span>
                                 </div>
 
                                 <div class="form-outline mb-2">
@@ -180,6 +183,7 @@ if (isset($_SESSION['username'])) {
 
                                 <div class="form-outline mb-4">
                                     <input type="password" aria-describedby="password_help" id="register_expert_password" class="form-control" placeholder="Password" name="expert_password"/>
+                                    <span toggle="#register_expert_password" class="fa fa-fw fa-eye field-icon toggle-password"></span>
                                 </div>
 
                                 <div class="form-outline mb-2">
@@ -215,6 +219,9 @@ if (isset($_SESSION['username'])) {
             </div>
         </div>
     </div>
+    
+    <?php require_once('includes/footer.php'); ?>
+    
 </body>
 </html>
 
