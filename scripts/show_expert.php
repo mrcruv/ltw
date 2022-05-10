af70b13914ce5621556ebc4508470378e8072ab1
Marco Ruvolo <marco.ruvolo@protonmail.com>
2022-05-10 08:08:15 +0200

    Update show_expert.php
    aggiunte funzioni di show competenze e titoli di un esperto

--- scripts/show_expert.php
+++ scripts/show_expert.php
@@ -18,3 +18,45 @@ function show_all_experts()
 //    require_once('includes/close_connection.php');
     return $rows;
 }
+
+function show_expert_title($username)
+{
+    global $connection;
+    $query = 'SELECT esperto, titolo, data_conseguimento, note, voto FROM titoli_esperti WHERE esperto = ?';
+    $statement = mysqli_prepare($connection, $query) or die(mysqli_error($connection));
+    mysqli_stmt_bind_param($statement, 's', $username) or die(mysqli_error($connection));
+    mysqli_stmt_execute($statement) or die(mysqli_error($connection));
+    mysqli_stmt_bind_result($statement, $username, $title, $date, $notes, $grade);
+    $num_rows = mysqli_stmt_num_rows($statement);
+    $i = 0;
+    $rows = array();
+    while (mysqli_stmt_fetch($statement)) {
+        $rows[$i] = array('username' => $username, 'title' => $title, 'date' => $date, 'notes' => $notes, 'grade' => $grade);
+        $i += 1;
+    }
+    mysqli_stmt_free_result($statement);
+    mysqli_stmt_close($statement) or die(mysqli_error($connection));
+//    require_once('includes/close_connection.php');
+    return $rows;
+}
+
+function show_expert_competence($username)
+{
+    global $connection;
+    $query = 'SELECT esperto, competenza, descrizione FROM competenze_esperti WHERE esperto = ?';
+    $statement = mysqli_prepare($connection, $query) or die(mysqli_error($connection));
+    mysqli_stmt_bind_param($statement, 's', $username) or die(mysqli_error($connection));
+    mysqli_stmt_execute($statement) or die(mysqli_error($connection));
+    mysqli_stmt_bind_result($statement, $username, $competence, $description);
+    $num_rows = mysqli_stmt_num_rows($statement);
+    $i = 0;
+    $rows = array();
+    while (mysqli_stmt_fetch($statement)) {
+        $rows[$i] = array('username' => $username, 'competence' => $competence, 'description' => $description);
+        $i += 1;
+    }
+    mysqli_stmt_free_result($statement);
+    mysqli_stmt_close($statement) or die(mysqli_error($connection));
+//    require_once('includes/close_connection.php');
+    return $rows;
+}
