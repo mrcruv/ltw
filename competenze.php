3a98c9efc8f127dd76b6a610968fed093b721b94
Marco Ruvolo <marco.ruvolo@protonmail.com>
2022-05-10 08:45:59 +0200

    Update some main pages
    aggiunte inclusioni script validazione form nell'header html

--- competenze.php
+++ competenze.php
@@ -20,6 +20,8 @@ $usertype = $_SESSION['usertype'];
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.min.js"></script>
+    <script src="scripts/validate_add_competence.js"></script>
 
     <title><?php echo($sitename); ?></title>
 </head>
--- processi.php
+++ processi.php
@@ -18,6 +18,8 @@ if (!isset($_SESSION['usertype']) or $_SESSION['usertype'] != 'ente') {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.min.js"></script>
+    <script src="scripts/validate_add_process.js"></script>
 
     <title><?php echo($sitename); ?></title>
 </head>
