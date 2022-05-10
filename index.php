da4c937b5856403409517a73d3c6491f7deb2b10
deto235 <102602333+deto235@users.noreply.github.com>
2022-05-10 12:47:12 +0200

    Update index.php
    Aggiunto alert con messaggio di errore

--- index.php
+++ index.php
@@ -27,6 +27,7 @@ if (isset($_SESSION['username'])) {
     <script src="scripts/form_switch.js"></script>
     <script src="scripts/validate_register_entity.js"></script>
     <script src="scripts/validate_register_expert.js"></script>
+    <script src="scripts/error_login.js"></script>
 
     <title><?php echo($sitename); ?></title>
 </head>
@@ -34,6 +35,27 @@ if (isset($_SESSION['username'])) {
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
+<?php
+    if (isset($_GET['err'])) {?>
+            <script>error();</script>
+<?php } ?>
+
+    <div class="modal fade" id="errorModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Errore</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <p><?php echo strtoupper($_GET['err']); ?></p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
 
 <div class="content">
     <div class="container">
