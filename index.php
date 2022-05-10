3412a0ba6e5da04c32d49a0e8b5d2cf85f0f86e2
deto235 <102602333+deto235@users.noreply.github.com>
2022-05-10 23:02:26 +0200

    Update index.php
    fix footer

--- index.php
+++ index.php
@@ -34,7 +34,7 @@ if (isset($_SESSION['username'])) {
 
     <title><?php echo($sitename); ?></title>
 </head>
-<body>
+<body class="d-flex flex-column min-vh-100">
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
