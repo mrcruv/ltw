d4830aae22c1dbc81602fc9b0b6159272949e4d0
Marco Ruvolo <marco.ruvolo@protonmail.com>
2022-05-16 21:59:58 +0200

    Update show_expert.php
    aggiunta funzione per verificare l'esistenza di un esperto

--- scripts/show_expert.php
+++ scripts/show_expert.php
@@ -6,7 +6,7 @@ function show_all_experts()
 {
     global $connection;
     $query = 'SELECT username, nome, cognome, citta_nascita, data_nascita FROM esperti';
-    $result =  mysqli_query($connection, $query);
+    $result =  mysqli_query($connection, $query) or die(mysqli_error($connection));
     $rows = array();
     $num_rows = mysqli_num_rows($result);
     $i = 0;
@@ -47,7 +47,7 @@ function show_expert_competence($username)
     $statement = mysqli_prepare($connection, $query) or die(mysqli_error($connection));
     mysqli_stmt_bind_param($statement, 's', $username) or die(mysqli_error($connection));
     mysqli_stmt_execute($statement) or die(mysqli_error($connection));
-    mysqli_stmt_bind_result($statement, $username, $competence, $description);
+    mysqli_stmt_bind_result($statement, $username, $competence, $description) or die(mysqli_error($connection));
     $num_rows = mysqli_stmt_num_rows($statement);
     $i = 0;
     $rows = array();
@@ -59,4 +59,18 @@ function show_expert_competence($username)
     mysqli_stmt_close($statement) or die(mysqli_error($connection));
 //    require_once('includes/close_connection.php');
     return $rows;
+}
+
+function expert_exists($username)
+{
+    global $connection;
+    $query = 'SELECT * FROM esperti WHERE username = ?';
+    $statement = mysqli_prepare($connection, $query) or die(mysqli_error($connection));
+    mysqli_stmt_bind_param($statement, 's', $username) or die(mysqli_error($connection));
+    mysqli_stmt_execute($statement) or die(mysqli_error($connection));
+    $exists = mysqli_stmt_fetch($statement);
+    mysqli_stmt_free_result($statement);
+    mysqli_stmt_close($statement) or die(mysqli_error($connection));
+//    require_once('includes/close_connection.php');
+    return $exists;
 }
