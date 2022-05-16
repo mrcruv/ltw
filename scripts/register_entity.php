1adb9bc295f0db79a82fb3385cc07f3733b04630
Marco Ruvolo <marco.ruvolo@protonmail.com>
2022-05-16 19:34:12 +0200

    Update register scripts
    fix maggiori (validazione form)

--- scripts/register_entity.php
+++ scripts/register_entity.php
@@ -16,45 +16,94 @@ $piva_regex = '/^[0-9]{11}$/';
 $website_regex = '/^((https?|ftp|smtp):\/\/)?(www.)?[a-z0-9]+\.[a-z]+(\/[a-zA-Z0-9#]+\/?)*$/';
 $name_regex = '/^[a-zA-Z0-9]{1,30}$/';
 
-$username = trim($_POST['entity_username']);
-$password = trim($_POST['entity_password']);
-$cf = strtoupper(trim($_POST['entity_cf']));
-$pec = strtolower(trim($_POST['entity_pec']));
-$piva = trim($_POST['entity_piva']);
-$website = strtolower(trim($_POST['entity_website']));
-$company_name = trim($_POST['entity_name']);
-$type = strtolower(trim($_POST['type']));
-$accept_conditions = isset($_POST['entity_term']) ? $_POST['entity_term'] : 'no';
+$username = isset($_POST['entity_username']) ? trim($_POST['entity_username']) : false;
+$password = isset($_POST['entity_password']) ? trim($_POST['entity_password']) : false;
+$cf = isset($_POST['entity_cf']) ? strtoupper(trim($_POST['entity_cf'])) : false;
+$pec = isset($_POST['entity_pec']) ? strtolower(trim($_POST['entity_pec'])) : false;
+$piva = isset($_POST['entity_piva']) ? trim($_POST['entity_piva']) : false;
+$website = isset($_POST['entity_website']) ? strtolower(trim($_POST['entity_website'])) : false;
+$company_name = isset($_POST['entity_name']) ? trim($_POST['entity_name']) : false;
+$type = isset($_POST['type']) ? strtolower(trim($_POST['type'])) : false;
+$accept_conditions = isset($_POST['entity_term']) ? strtolower(trim($_POST['entity_term'])) : false;
 
-!empty($username) or header('Location: ../index.php?err=username+non+inserito');
-preg_match($username_regex, $username) or header('Location: ../index.php?err=username+non+corretto');
+if (empty($username)) {
+    header('Location: ../index.php?err=username+non+inserito');
+    die('username non inserito');
+}
+if (!preg_match($username_regex, $username)) {
+    header('Location: ../index.php?err=username+non+corretto');
+    die('username non corretto');
+}
 
-!empty($password) or header('Location: ../index.php?err=password+non+inserita');
+if (empty($password)) {
+    header('Location: ../index.php?err=password+non+inserita');
+    die('password non inserita');
+}
 $msg = '';
-strlen($password) >= 8 or $msg = $msg . 'lunghezza+minima+non+raggiunta';
-preg_match($contains_lowercase, $password) or $msg = $msg . 'lowercase+non+incluso';
-preg_match($contains_special, $password) or $msg = $msg . 'special+non+incluso';
-preg_match($contains_uppercase, $password) or $msg = $msg . 'uppercase+non+incluso';
-preg_match($contains_digit, $password) or $msg = $msg . 'digit+non+inclusa';
-$msg == '' or header('Location: ../index.php?err=' . $msg);
+strlen($password) >= 8 or $msg .= 'lunghezza+minima+non+raggiunta';
+preg_match($contains_lowercase, $password) or $msg .= 'lowercase+non+incluso';
+preg_match($contains_special, $password) or $msg .= 'special+non+incluso';
+preg_match($contains_uppercase, $password) or $msg .= 'uppercase+non+incluso';
+preg_match($contains_digit, $password) or $msg .= 'digit+non+inclusa';
+if ($msg != '') {
+    header('Location: ../index.php?err=' . $msg);
+    die(str_replace('+', ' ', $msg));
+}
 
-!empty($cf) or header('Location: ../index.php?err=c.f.+non+inserito');
-preg_match($cf_regex, $cf) or header('Location: ../index.php?err=c.f.+non+corretto');
+if (empty($cf)) {
+    header('Location: ../index.php?err=c.f.+non+inserito');
+    die('c.f. non inserito');
+}
+if (!preg_match($cf_regex, $cf)) {
+    header('Location: ../index.php?err=c.f.+non+corretto');
+    die('c.f. non corretto');
+}
 
-!empty($pec) or header('Location: ../index.php?err=pec+non+inserita');
-preg_match($pec_regex, $pec) or header('Location: ../index.php?err=pec+non+corretta');
+if (empty($pec)) {
+    header('Location: ../index.php?err=pec+non+inserita');
+    die('pec non inserita');
+}
+if (!preg_match($pec_regex, $pec)) {
+    header('Location: ../index.php?err=pec+non+corretta');
+    die('pec non corretta');
+}
 
-!empty($piva) or header('Location: ../index.php?err=p.+iva+non+inserita');
-preg_match($piva_regex, $piva) or header('Location: ../index.php?err=p.+iva+non+corretta');
+if (empty($piva)) {
+    header('Location: ../index.php?err=p.+iva+non+inserita');
+    die('p. iva non inserita');
+}
+if (!preg_match($piva_regex, $piva)) {
+    header('Location: ../index.php?err=p.+iva+non+corretta');
+    die('p. iva non corretta');
+}
 
-if (!empty($website)) preg_match($website_regex, $website) or header('Location: ../index.php?err=sito+web+non+corretto');
+if (!empty($website) and !preg_match($website_regex, $website)) {
+    header('Location: ../index.php?err=sito+web+non+corretto');
+    die('sito web non corretto');
+}
 
-!empty($company_name) or header('Location: ../index.php?err=denominazione+non+inserita');
-preg_match($name_regex, $company_name) or header('Location: ../index.php?err=denominazione+non+corretta');
+if (empty($company_name)) {
+    header('Location: ../index.php?err=denominazione+non+inserita');
+    die('denominazione non inserita');
+}
+if (!preg_match($name_regex, $company_name)) {
+    header('Location: ../index.php?err=denominazione+non+corretta');
+    die('denominazione non corretta');
+}
 
-($type == 'pubblico' or $type == 'privato') or header('Location: ../index.php?err=tipo+non+corretto');
+if (empty($type)) {
+    header('Location: ../index.php?err=tipo+non+inserito');
+    die('tipo non inserito');
+}
+if ($type != 'pubblico' and $type != 'privato') {
+    header('Location: ../index.php?err=tipo+non+corretto');
+    die('tipo non corretto');
+}
 
-($accept_conditions == 'yes') or header('Location: ../index.php?err=condizioni+non+accettate');
+if (empty($accept_conditions) or ($accept_conditions != 'true')){
+    header('Location: ../index.php?err=condizioni+non+accettate');
+    die('condizioni non accettate');
+}
 
 $query = 'SELECT * FROM utenti WHERE username=? OR pec=? OR cf=? OR piva=?';
 $statement = mysqli_prepare($connection, $query) or die(mysqli_error($connection));
@@ -62,8 +111,8 @@ mysqli_stmt_bind_param($statement, 'ssss', $username, $pec, $cf, $piva) or die(m
 mysqli_stmt_execute($statement) or die(mysqli_error($connection));
 if (mysqli_stmt_fetch($statement)) {
         mysqli_stmt_close($statement) or die(mysqli_error($connection));
-        echo('utente già esistente');
         header('Location: ../index.php?err=utente+gia+esistente');
+        die('utente già esistente');
 }
 mysqli_stmt_close($statement) or die(mysqli_error($connection));
 
@@ -84,4 +133,4 @@ mysqli_stmt_execute($statement) or die(mysqli_error($connection));
 mysqli_stmt_close($statement) or die(mysqli_error($connection));
 
 //require_once('../includes/close_connection.php');
-header('Location: ../index.php?msg=utente+registrato+con+successo');
+header('Location: ../index.php?msg=utente+ente+registrato+con+successo');
--- scripts/register_expert.php
+++ scripts/register_expert.php
@@ -15,56 +15,117 @@ $pec_regex = '/(?:\w*.?pec(?:.?\w+)*)/';
 $piva_regex = '/^[0-9]{11}$/';
 $website_regex = '/^((https?|ftp|smtp):\/\/)?(www.)?[a-z0-9]+\.[a-z]+(\/[a-zA-Z0-9#]+\/?)*$/';
 $name_regex = '/^[a-zA-Z0-9]{1,30}$/';
+$surname_regex = $name_regex;
 //$date_regex = '';
 
-$username = trim($_POST['expert_username']);
-$password = trim($_POST['expert_password']);
-$cf = strtoupper(trim($_POST['expert_cf']));
-$pec = strtolower(trim($_POST['expert_pec']));
-$piva = trim($_POST['expert_piva']);
-$website = strtolower(trim($_POST['expert_website']));
-$accept_conditions = isset($_POST['expert_term']) ? $_POST['expert_term'] : 'no';
-$name = trim($_POST['name']);
-$surname = trim($_POST['surname']);
-$city = trim($_POST['city']);
-$date = $_POST['date'];
+$username = isset($_POST['expert_username']) ? trim($_POST['expert_username']) : false;
+$password = isset($_POST['expert_password']) ? trim($_POST['expert_password']) : false;
+$cf = isset($_POST['expert_cf']) ? strtoupper(trim($_POST['expert_cf'])) : false;
+$pec = isset($_POST['expert_pec']) ? strtolower(trim($_POST['expert_pec'])) : false;
+$piva = isset($_POST['expert_piva']) ? trim($_POST['expert_piva']) : false;
+$website = isset($_POST['expert_website']) ? strtolower(trim($_POST['expert_website'])) :false;
+$accept_conditions = isset($_POST['expert_term']) ? strtolower(trim($_POST['expert_term'])) : false;
+$name = isset($_POST['name']) ? trim($_POST['name']) : false;
+$surname = isset($_POST['surname']) ? trim($_POST['surname']) : false;
+$city = isset($_POST['city']) ? trim($_POST['city']) : false;
+$date = isset($_POST['date']) ? trim($_POST['date']) : false;
 
-!empty($username) or header('Location: ../index.php?err=username+non+inserito');
-preg_match($username_regex, $username) or header('Location: ../index.php?err=username+non+corretto');
+if (empty($username)) {
+    header('Location: ../index.php?err=username+non+inserito');
+    die('username non inserito');
+}
+if (!preg_match($username_regex, $username)) {
+    header('Location: ../index.php?err=username+non+corretto');
+    die('username non corretto');
+}
 
-!empty($password) or header('Location: ../index.php?err=password+non+inserita');
+if (empty($password)) {
+    header('Location: ../index.php?err=password+non+inserita');
+    die('password non inserita');
+}
 $msg = '';
-strlen($password) >= 8 or $msg = $msg . 'lunghezza+minima+non+raggiunta';
-preg_match($contains_lowercase, $password) or $msg = $msg . 'lowercase+non+incluso';
-preg_match($contains_special, $password) or $msg = $msg . 'special+non+incluso';
-preg_match($contains_uppercase, $password) or $msg = $msg . 'uppercase+non+incluso';
-preg_match($contains_digit, $password) or $msg = $msg . 'digit+non+inclusa';
-$msg == '' or header('Location: ../index.php?err=' . $msg);
+strlen($password) >= 8 or $msg .= 'lunghezza+minima+non+raggiunta';
+preg_match($contains_lowercase, $password) or $msg .= 'lowercase+non+incluso';
+preg_match($contains_special, $password) or $msg .= 'special+non+incluso';
+preg_match($contains_uppercase, $password) or $msg .= 'uppercase+non+incluso';
+preg_match($contains_digit, $password) or $msg .= 'digit+non+inclusa';
+if ($msg != '') {
+    header('Location: ../index.php?err=' . $msg);
+    die(str_replace('+', ' ', $msg));
+}
 
-!empty($cf) or header('Location: ../index.php?err=c.f.+non+inserito');
-preg_match($cf_regex, $cf) or header('Location: ../index.php?err=c.f.+non+corretto');
+if (empty($cf)) {
+    header('Location: ../index.php?err=c.f.+non+inserito');
+    die('c.f. non inserito');
+}
+if (!preg_match($cf_regex, $cf)) {
+    header('Location: ../index.php?err=c.f.+non+corretto');
+    die('c.f. non corretto');
+}
 
-!empty($pec) or header('Location: ../index.php?err=pec+non+inserita');
-preg_match($pec_regex, $pec) or header('Location: ../index.php?err=pec+non+corretta');
+if (empty($pec)) {
+    header('Location: ../index.php?err=pec+non+inserita');
+    die('pec non inserita');
+}
+if (!preg_match($pec_regex, $pec)) {
+    header('Location: ../index.php?err=pec+non+corretta');
+    die('pec non corretta');
+}
 
-!empty($piva) or header('Location: ../index.php?err=p.+iva+non+inserita');
-preg_match($piva_regex, $piva) or header('Location: ../index.php?err=p.+iva+non+corretta');
+if (empty($piva)) {
+    header('Location: ../index.php?err=p.+iva+non+inserita');
+    die('p. iva non inserita');
+}
+if (!preg_match($piva_regex, $piva)) {
+    header('Location: ../index.php?err=p.+iva+non+corretta');
+    die('p. iva non corretta');
+}
 
-if (!empty($website)) preg_match($website_regex, $website) or header('Location: ../index.php?err=sito+web+non+corretto');
+if (!empty($website) and !preg_match($website_regex, $website)) {
+    header('Location: ../index.php?err=sito+web+non+corretto');
+    die('sito web non corretto');
+}
 
-!empty($name) or header('Location: ../index.php?err=nome+non+inserito');
-preg_match($name_regex, $name) or  header('Location: ../index.php?err=nome+non+corretto');
+if (empty($name)){
+    header('Location: ../index.php?err=nome+non+inserito');
+    die('nome non inserito');
+}
+if (!preg_match($name_regex, $name)){
+    header('Location: ../index.php?err=nome+non+corretto');
+    die('nome non corretto');
+}
 
-!empty($surname) or header('Location: ../index.php?err=cognome+non+inserito');
-preg_match($name_regex, $surname) or header('Location: ../index.php?err=cognome+non+corretto');
+if (empty($surname)){
+    header('Location: ../index.php?err=cognome+non+inserito');
+    die('cognome non inserito');
+}
+if (!preg_match($surname_regex, $surname)){
+    header('Location: ../index.php?err=cognome+non+corretto');
+    die('cognome non corretto');
+}
 
-!empty($city) or header('Location: ../index.php?err=città+non+inserita');
-preg_match($name_regex, $city) or header('Location: ../index.php?err=città+non+corretta');
+if (empty($city)){
+    header('Location: ../index.php?err=città+non+inserita');
+    die('città non inserita');
+}
+if (!preg_match($name_regex, $city)){
+    header('Location: ../index.php?err=città+non+corretta');
+    die('città non corretta');
+}
 
-//!empty($date) or die('data non inserita');
-//preg_match($date_regex, $date) or die('data non corretta');
+//if (empty($date)) {
+//    header('Location: ../index.php?err=data+non+inserita');
+//    die('data non inserita');
+//}
+//if (!preg_match($date_regex, $date)){
+//    header('Location: ../index.php?err=data+non+corretta');
+//    die('data non corretta');
+//}
 
-($accept_conditions == 'yes') or header('Location: ../index.php?err=condizioni+non+accettate');
+if (empty($accept_conditions) or ($accept_conditions != 'true')){
+    header('Location: ../index.php?err=condizioni+non+accettate');
+    die('condizioni non accettate');
+}
 
 $query = 'SELECT * FROM utenti WHERE username=? OR pec=? OR cf=? OR piva=?';
 $statement = mysqli_prepare($connection, $query) or die(mysqli_error($connection));
@@ -72,8 +133,8 @@ mysqli_stmt_bind_param($statement, 'ssss', $username, $pec, $cf, $piva) or die(m
 mysqli_stmt_execute($statement) or die(mysqli_error($connection));
 if (mysqli_stmt_fetch($statement)) {
     mysqli_stmt_close($statement) or die(mysqli_error($connection));
-    echo('utente già esistente');
     header('Location: ../index.php?err=utente+gia+esistente');
+    die('utente già esistente');
 }
 mysqli_stmt_close($statement) or die(mysqli_error($connection));
 
@@ -96,4 +157,4 @@ mysqli_stmt_close($statement) or die(mysqli_error($connection));
 
 
 //require_once('../includes/close_connection.php');
-header('Location: ../index.php?msg=utente+registrato+con+successo');
+header('Location: ../index.php?msg=utente+esperto+registrato+con+successo');
