b6266c7bdce07e01aad60f7b15201e19ada6bafd
deto235 <102602333+deto235@users.noreply.github.com>
2022-05-16 17:55:51 +0200

    Update register_expert.php
    aggiunto validation come su register entity

--- scripts/register_expert.php
+++ scripts/register_expert.php
@@ -29,42 +29,42 @@ $surname = trim($_POST['surname']);
 $city = trim($_POST['city']);
 $date = $_POST['date'];
 
-!empty($username) or die('username non inserito');
-preg_match($username_regex, $username) or die('username non corretto');
+!empty($username) or header('Location: ../index.php?err=username+non+inserito');
+preg_match($username_regex, $username) or header('Location: ../index.php?err=username+non+corretto');
 
-!empty($password) or die('password non inserita');
+!empty($password) or header('Location: ../index.php?err=password+non+inserita');
 $msg = '';
-strlen($password) >= 8 or $msg = $msg . 'lunghezza minima non raggiunta ';
-preg_match($contains_lowercase, $password) or $msg = $msg . 'lowercase non incluso ';
-preg_match($contains_special, $password) or $msg = $msg . 'special non incluso';
-preg_match($contains_uppercase, $password) or $msg = $msg . 'uppercase non incluso ';
-preg_match($contains_digit, $password) or $msg = $msg . 'digit non inclusa';
-$msg == '' or die($msg);
+strlen($password) >= 8 or $msg = $msg . 'lunghezza+minima+non+raggiunta';
+preg_match($contains_lowercase, $password) or $msg = $msg . 'lowercase+non+incluso';
+preg_match($contains_special, $password) or $msg = $msg . 'special+non+incluso';
+preg_match($contains_uppercase, $password) or $msg = $msg . 'uppercase+non+incluso';
+preg_match($contains_digit, $password) or $msg = $msg . 'digit+non+inclusa';
+$msg == '' or header('Location: ../index.php?err=' . $msg);
 
-!empty($cf) or die('c.f. non inserito');
-preg_match($cf_regex, $cf) or die('cf non corretto');
+!empty($cf) or header('Location: ../index.php?err=c.f.+non+inserito');
+preg_match($cf_regex, $cf) or header('Location: ../index.php?err=c.f.+non+corretto');
 
-!empty($pec) or die('pec non inserita');
-preg_match($pec_regex, $pec) or die('pec non corretta');
+!empty($pec) or header('Location: ../index.php?err=pec+non+inserita');
+preg_match($pec_regex, $pec) or header('Location: ../index.php?err=pec+non+corretta');
 
-!empty($piva) or die('p. iva non inserita');
-preg_match($piva_regex, $piva) or die('p. iva non corretta');
+!empty($piva) or header('Location: ../index.php?err=p.+iva+non+inserita');
+preg_match($piva_regex, $piva) or header('Location: ../index.php?err=p.+iva+non+corretta');
 
-($accept_conditions == 'yes') or die('condizioni non accettate');
+if (!empty($website)) preg_match($website_regex, $website) or header('Location: ../index.php?err=sito+web+non+corretto');
 
-!empty($name) or die('nome non inserita');
-preg_match($name_regex, $name) or die('nome non corretta');
+!empty($name) or header('Location: ../index.php?err=nome+non+inserito');
+preg_match($name_regex, $name) or  header('Location: ../index.php?err=nome+non+corretto');
 
-!empty($surname) or die('cognome non inserito');
-preg_match($name_regex, $surname) or die('cognome non corretto');
+!empty($surname) or header('Location: ../index.php?err=cognome+non+inserito');
+preg_match($name_regex, $surname) or header('Location: ../index.php?err=cognome+non+corretto');
 
-if (!empty($website)) preg_match($website_regex, $website) or die('sito web non corretto');
-
-!empty($city) or die('città non inserita');
-preg_match($name_regex, $city) or die('città non corretta');
+!empty($city) or header('Location: ../index.php?err=città+non+inserita');
+preg_match($name_regex, $city) or header('Location: ../index.php?err=città+non+corretta');
 
 //!empty($date) or die('data non inserita');
 //preg_match($date_regex, $date) or die('data non corretta');
+
+($accept_conditions == 'yes') or header('Location: ../index.php?err=condizioni+non+accettate');
 
 $query = 'SELECT * FROM utenti WHERE username=? OR pec=? OR cf=? OR piva=?';
 $statement = mysqli_prepare($connection, $query) or die(mysqli_error($connection));
