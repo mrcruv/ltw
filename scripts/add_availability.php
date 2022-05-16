a10ede3ade77451f5adc37148d59ecaf089d4364
Marco Ruvolo <marco.ruvolo@protonmail.com>
2022-05-16 11:55:06 +0200

    Update add_availability.php
    aggiunta validazione

--- scripts/add_availability.php
+++ scripts/add_availability.php
@@ -10,6 +10,35 @@ $username = $_SESSION['username'];
 $expert = trim($_POST['expert']);
 $process = trim($_POST['process']);
 
+if ($process == 'empty') {
+    header ('Location: ../assegnazioni.php?err=selezionare+processo');
+    die("selezionare processo");
+}
+if ($expert == 'empty') {
+    header ('Location: ../assegnazioni.php?err=selezionare+esperto');
+    die("selezionare esperto");
+}
+
+$query = 'SELECT * FROM esperti WHERE username=?';
+$statement = mysqli_prepare($connection, $query) or die(mysqli_error($connection));
+mysqli_stmt_bind_param($statement, 's', $expert) or die(mysqli_error($connection));
+mysqli_stmt_execute($statement) or die(mysqli_error($connection));
+if (!mysqli_stmt_fetch($statement)) {
+    mysqli_stmt_close($statement) or die(mysqli_error($connection));
+    header ('Location: ../assegnazioni.php?err=esperto+non+esistente');
+}
+mysqli_stmt_close($statement) or die(mysqli_error($connection));
+
+$query = 'SELECT * FROM processi WHERE nome=?';
+$statement = mysqli_prepare($connection, $query) or die(mysqli_error($connection));
+mysqli_stmt_bind_param($statement, 's', $process) or die(mysqli_error($connection));
+mysqli_stmt_execute($statement) or die(mysqli_error($connection));
+if (!mysqli_stmt_fetch($statement)) {
+    mysqli_stmt_close($statement) or die(mysqli_error($connection));
+    header ('Location: ../assegnazioni.php?err=processo+non+esistente');
+}
+mysqli_stmt_close($statement) or die(mysqli_error($connection));
+
 $query = 'SELECT * FROM disponibilita WHERE processo=? AND esperto=?';
 $statement = mysqli_prepare($connection, $query) or die(mysqli_error($connection));
 mysqli_stmt_bind_param($statement, 'ss', $process, $expert) or die(mysqli_error($connection));
