6d867b314a1481920b705204987e34f326dc0359
Marco Ruvolo <marco.ruvolo@protonmail.com>
2022-05-20 08:29:47 +0200

    Create delete scripts
    creazione script di eliminazione titoli e competenze

---
+++ scripts/delete_competence.php
@@ -0,0 +1,51 @@
+<?php
+global $connection;
+require_once('../includes/open_connection.php');
+require_once('../includes/session.php');
+if (!isset($_SESSION['usertype']) or $_SESSION['usertype'] != 'esperto') {
+    header ('Location: ../competenze.php');
+}
+
+$username = $_SESSION['username'];
+$name = isset($_GET['name']) ? trim($_GET['name']) : false;
+$area = isset($_GET['area']) ? trim($_GET['area']) : false;
+
+if (empty($name)){
+    header('Location: ../competenze.php?err=nome+competenza+non+selezionato');
+    die('nome competenza non selezionato');
+}
+if (empty($area)){
+    header('Location: ../competenze.php?err=area+competenza+non+selezionata');
+    die('area competenza non selezionata');
+}
+
+$query = 'SELECT * FROM competenze WHERE nome=? AND settore=?';
+$statement = mysqli_prepare($connection, $query) or die(mysqli_error($connection));
+mysqli_stmt_bind_param($statement, 'ss', $name, $area) or die(mysqli_error($connection));
+mysqli_stmt_execute($statement) or die(mysqli_error($connection));
+if (!mysqli_stmt_fetch($statement)) {
+    mysqli_stmt_close($statement) or die(mysqli_error($connection));
+    header('Location: ../competenze.php?err=competenza+non+esistente');
+    die('competenza non esistente');
+}
+mysqli_stmt_close($statement) or die(mysqli_error($connection));
+
+$query = 'SELECT * FROM competenze_esperti WHERE esperto=? AND competenza=? AND settore=?';
+$statement = mysqli_prepare($connection, $query) or die(mysqli_error($connection));
+mysqli_stmt_bind_param($statement, 'sss', $username, $name, $area) or die(mysqli_error($connection));
+mysqli_stmt_execute($statement) or die(mysqli_error($connection));
+if (!mysqli_stmt_fetch($statement)) {
+    mysqli_stmt_close($statement) or die(mysqli_error($connection));
+    header('Location: ../competenze.php?err=competenza+non+inserita');
+    die('competenza non inserita');
+}
+mysqli_stmt_close($statement) or die(mysqli_error($connection));
+
+$query = 'DELETE FROM competenze_esperti WHERE esperto=? AND competenza=? AND settore=?';
+$statement = mysqli_prepare($connection, $query) or die(mysqli_error($connection));
+mysqli_stmt_bind_param($statement, 'sss', $username, $name, $area) or die(mysqli_error($connection));
+mysqli_stmt_execute($statement) or die(mysqli_error($connection));
+mysqli_stmt_close($statement) or die(mysqli_error($connection));
+
+//require_once("../includes/close_connection.php");
+header('Location: ../competenze.php?msg=competenza+eliminata+con+successo');
---
+++ scripts/delete_title.php
@@ -0,0 +1,46 @@
+<?php
+global $connection;
+require_once('../includes/open_connection.php');
+require_once('../includes/session.php');
+if (!isset($_SESSION['usertype']) or $_SESSION['usertype'] != 'esperto') {
+    header ('Location: ../titoli.php');
+}
+
+$username = $_SESSION['username'];
+$name = isset($_GET['name']) ? trim($_GET['name']) : false;
+
+if (empty($name)){
+    header('Location: ../titoli.php?err=titolo+non+selezionato');
+    die('titolo non selezionato');
+}
+
+$query = 'SELECT * FROM titoli WHERE denominazione=?';
+$statement = mysqli_prepare($connection, $query) or die(mysqli_error($connection));
+mysqli_stmt_bind_param($statement, 's', $name) or die(mysqli_error($connection));
+mysqli_stmt_execute($statement) or die(mysqli_error($connection));
+if (!mysqli_stmt_fetch($statement)) {
+    mysqli_stmt_close($statement) or die(mysqli_error($connection));
+    header('Location: ../titoli.php?err=titolo+non+esistente');
+    die('titolo non esistente');
+}
+mysqli_stmt_close($statement) or die(mysqli_error($connection));
+
+$query = 'SELECT * FROM titoli_esperti WHERE esperto=? AND titolo=?';
+$statement = mysqli_prepare($connection, $query) or die(mysqli_error($connection));
+mysqli_stmt_bind_param($statement, 'ss', $username, $name) or die(mysqli_error($connection));
+mysqli_stmt_execute($statement) or die(mysqli_error($connection));
+if (!mysqli_stmt_fetch($statement)) {
+    mysqli_stmt_close($statement) or die(mysqli_error($connection));
+    header('Location: ../titoli.php?err=titolo+non+inserito');
+    die('titolo non inserito');
+}
+mysqli_stmt_close($statement) or die(mysqli_error($connection));
+
+$query = 'DELETE FROM titoli_esperti WHERE esperto=? AND titolo=?';
+$statement = mysqli_prepare($connection, $query) or die(mysqli_error($connection));
+mysqli_stmt_bind_param($statement, 'ss', $username, $name) or die(mysqli_error($connection));
+mysqli_stmt_execute($statement) or die(mysqli_error($connection));
+mysqli_stmt_close($statement) or die(mysqli_error($connection));
+
+//require_once("../includes/close_connection.php");
+header('Location: ../titoli.php?msg=titolo+eliminato+con+successo');
