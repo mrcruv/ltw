28de7f6e379c6cc1f7ce02bf0ae1e830dcd6ae85
deto235 <102602333+deto235@users.noreply.github.com>
2022-05-21 08:40:03 +0200

    Update competenze.php
    fix grafici

--- competenze.php
+++ competenze.php
@@ -18,6 +18,8 @@ $usertype = $_SESSION['usertype'];
     <meta name="author" content=""/>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="css/style.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.min.js"></script>
@@ -52,7 +54,7 @@ $usertype = $_SESSION['usertype'];
         <div class="row">
             <div class="col-4 offset-4 text-center align-middle mb-2">
                 <h5>Aggiungi competenza</h5>
-                <a class="btn btn-primary rounded-circle" id="add_button">+</a>
+                <a type="button" class="btn btn-info rounded-circle" id="add_button"><i class="fa fa-plus"></i></a>
 
                 <form id="add_competence_form" class="add_form" action="scripts/add_competence.php" method="post">
                     <div class="form-group mb-3 mt-4">
@@ -69,37 +71,44 @@ $usertype = $_SESSION['usertype'];
             </div>
         </div>
 
-        <table class="table table-striped">
-            <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Competenza</th>
-                    <th scope="col">Settore</th>
-                    <th scope="col">Descrizione</th>
-                    <th scope="col">X</th>
-                </tr>
-            </thead>
-            <tbody>
-            <?php $array = show_all_competences($_SESSION['username']);
-            $n = count($array);
-            if (!is_array($array) or $n <= 0): ?>
-                <tr><td colspan="4"><h6>Non ci sono Competenze al momento</h6></td></tr>
-            <?php else:
-                for ($i = 0; $i < $n; $i += 1) { ?>
-                <tr>
-                    <th scope="row"><?php echo($i+1); ?></th>
-                    <td><?php echo($array[$i]['name']); ?></td>
-                    <td><?php echo($array[$i]['area']); ?></td>
-                    <td><?php echo($array[$i]['description']); ?></td>
-                    <?php
-                    echo('<td><a href="scripts/delete_competence.php?name=' . $array[$i]['name'] . '&area=' . $array[$i]['area'] . '">');
-                    echo('<button type="button">' . 'X' . '</button></a>');
-                    ?>
-                </tr>
-                <?php } ?>
-            <?php endif; ?>
-            </tbody>
-        </table>
+        <div class="row mt-2">
+            <div class="col-6 offset-3">
+
+                <table class="table table-striped rounded-3 shadow">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Competenza</th>
+                            <th scope="col">Settore</th>
+                            <th scope="col">Descrizione</th>
+                            <th class="text-center" scope="col">Azioni</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    <?php $array = show_all_competences($_SESSION['username']);
+                    $n = count($array);
+                    if (!is_array($array) or $n <= 0): ?>
+                        <tr><td colspan="5"><h6>Non ci sono Competenze al momento</h6></td></tr>
+                    <?php else:
+                        for ($i = 0; $i < $n; $i += 1) { ?>
+                        <tr>
+                            <th scope="row"><?php echo($i+1); ?></th>
+                            <td><?php echo($array[$i]['name']); ?></td>
+                            <td><?php echo($array[$i]['area']); ?></td>
+                            <td><?php echo($array[$i]['description']); ?></td>
+                            <td class="text-center">
+                            <?php
+                            echo('<a class="delete" title="Delete" data-toggle="tooltip" href="scripts/delete_competence.php?name=' . $array[$i]['name'] . '&area=' . $array[$i]['area'] . '">');
+                            echo('<i class="material-icons icon-red">&#xE872;</i></a>');;
+                            ?>
+                            </td>
+                        </tr>
+                        <?php } ?>
+                    <?php endif; ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
 
     <?php require_once('includes/footer.php'); ?>
