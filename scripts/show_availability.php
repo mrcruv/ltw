97092efbb716a9bf162203f576671e1ca4fa688e
Marco Ruvolo <marco.ruvolo@protonmail.com>
2022-05-23 09:14:27 +0200

    Update some show scripts
    aggiunti campi restituiti dalle query di show

--- scripts/show_availability.php
+++ scripts/show_availability.php
@@ -5,19 +5,18 @@ require_once('includes/session.php');
 function show_all_availabilities_from_expert($expert_username)
 {
     global $connection;
-    $query = 'SELECT processo, descrizione, tipologia, disponibilita.ente, data_richiesta, data_assegnazione, data_rifiuto, data_conclusione
-    FROM disponibilita JOIN processi ON
-    disponibilita.processo = processi.nome WHERE esperto=?';
+    $query = 'SELECT processo, descrizione, tipologia, pec, sito_web, disponibilita.ente, data_richiesta, data_assegnazione, data_rifiuto, data_conclusione
+    FROM disponibilita JOIN processi ON disponibilita.processo = processi.nome JOIN utenti ON disponibilita.ente = utenti.username WHERE esperto=?';
     $statement = mysqli_prepare($connection, $query) or die(mysqli_error($connection));
     mysqli_stmt_bind_param($statement, 's', $expert_username) or die(mysqli_error($connection));
     mysqli_stmt_execute($statement) or die(mysqli_error($connection));
-    mysqli_stmt_bind_result($statement, $process, $description, $type, $entity, $request_date, $allocation_date, $rejection_date, $conclusion_date) or die(mysqli_error($connection));
+    mysqli_stmt_bind_result($statement, $process, $description, $type, $pec, $website, $entity, $request_date, $allocation_date, $rejection_date, $conclusion_date) or die(mysqli_error($connection));
     $num_rows = mysqli_stmt_num_rows($statement);
     $i = 0;
     $rows = array();
     while (mysqli_stmt_fetch($statement)) {
-        $rows[$i] = array('process' => $process, 'description' => $description, 'type' => $type, 'entity' => $entity, 'request_date' => $request_date,
-            'allocation_date' => $allocation_date, 'rejection_date' => $rejection_date, 'conclusion_date' => $conclusion_date);
+        $rows[$i] = array('process' => $process, 'description' => $description, 'type' => $type, 'entity' => $entity, 'pec' => $pec, 'website' => $website,
+            'request_date' => $request_date, 'allocation_date' => $allocation_date, 'rejection_date' => $rejection_date, 'conclusion_date' => $conclusion_date);
         $i += 1;
     }
     mysqli_stmt_free_result($statement);
@@ -29,19 +28,19 @@ function show_all_availabilities_from_expert($expert_username)
 function show_all_availabilities_from_entity($entity_username)
 {
     global $connection;
-    $query = 'SELECT processo, descrizione, tipologia, esperto, data_richiesta, data_assegnazione, data_rifiuto, data_conclusione
-    FROM disponibilita JOIN processi ON
-    disponibilita.processo = processi.nome WHERE disponibilita.ente=?';
+    $query = 'SELECT processo, descrizione, tipologia, pec, sito_web, esperto, data_richiesta, data_assegnazione, data_rifiuto, data_conclusione
+    FROM disponibilita JOIN processi ON disponibilita.processo = processi.nome JOIN utenti ON disponibilita.esperto = utenti.username
+    WHERE disponibilita.ente=?';
     $statement = mysqli_prepare($connection, $query) or die(mysqli_error($connection));
     mysqli_stmt_bind_param($statement, 's', $entity_username) or die(mysqli_error($connection));
     mysqli_stmt_execute($statement) or die(mysqli_error($connection));
-    mysqli_stmt_bind_result($statement, $process, $description, $type, $expert, $request_date, $allocation_date, $rejection_date, $conclusion_date) or die(mysqli_error($connection));
+    mysqli_stmt_bind_result($statement, $process, $description, $type, $pec, $website, $expert, $request_date, $allocation_date, $rejection_date, $conclusion_date) or die(mysqli_error($connection));
     $num_rows = mysqli_stmt_num_rows($statement);
     $i = 0;
     $rows = array();
     while (mysqli_stmt_fetch($statement)) {
-        $rows[$i] = array('process' => $process, 'description' => $description, 'type' => $type, 'expert' => $expert, 'request_date' => $request_date,
-            'allocation_date' => $allocation_date, 'rejection_date' => $rejection_date, 'conclusion_date' => $conclusion_date);
+        $rows[$i] = array('process' => $process, 'description' => $description, 'type' => $type, 'expert' => $expert, 'pec' => $pec, 'website' => $website,
+            'request_date' => $request_date, 'allocation_date' => $allocation_date, 'rejection_date' => $rejection_date, 'conclusion_date' => $conclusion_date);
         $i += 1;
     }
     mysqli_stmt_free_result($statement);
--- scripts/show_expert.php
+++ scripts/show_expert.php
@@ -5,14 +5,14 @@ require_once('includes/session.php');
 function show_all_experts()
 {
     global $connection;
-    $query = 'SELECT username, nome, cognome, citta_nascita, data_nascita FROM esperti';
+    $query = 'SELECT username, nome, cognome, citta_nascita, data_nascita, sito_web, pec FROM esperti NATURAL JOIN utenti';
     $result =  mysqli_query($connection, $query) or die(mysqli_error($connection));
     $rows = array();
     $num_rows = mysqli_num_rows($result);
     $i = 0;
     while ($row = mysqli_fetch_row($result)) {
-        $rows[$i] = array('username' => $row['0'], 'name' => $row['1'], 'surname' => $row['2'],
-            'city' => $row['3'], 'date' => $row['4']);
+        $rows[$i] = array('username' => $row[0], 'name' => $row[1], 'surname' => $row[2],
+            'city' => $row[3], 'date' => $row[4], 'website' => $row[5], 'pec' => $row[6]);
         $i += 1;
     }
 //    require_once('includes/close_connection.php');
