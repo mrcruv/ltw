1374b133306bc9d1a5c8b8b7842f3235214bf0b5
Marco Ruvolo <marco.ruvolo@protonmail.com>
2022-05-24 00:02:03 +0200

    Update expert_stats.php
    fix query

--- scripts/expert_stats.php
+++ scripts/expert_stats.php
@@ -1,91 +1,120 @@
 <?php
 require_once('includes/session.php');
 require_once('includes/open_connection.php');
-if (!isset($_SESSION['usertype']) or $_SESSION['usertype'] != 'esperto') {
-    header ('Location: ../me.php?err=sessione+utente+esperto+non+attiva');
-    die('sessione utente esperto non attiva');
-}
 
-$username = $_SESSION['username'];
-
-function n_title()
+function n_title($username)
 {
-    global $connection, $username;
-    $query = 'SELECT COUNT(*) FROM titoli_esperti WHERE esperto = ?';
+    global $connection;
+    $n = 0;
+    $query = 'SELECT COUNT(*) AS n FROM titoli_esperti WHERE esperto = ?';
     $statement = mysqli_prepare($connection, $query) or die(mysqli_error($connection));
     mysqli_stmt_bind_param($statement, 's', $username) or die(mysqli_error($connection));
     mysqli_stmt_execute($statement) or die(mysqli_error($connection));
     mysqli_stmt_bind_result($statement, $n) or die(mysqli_error($connection));
+    if (!mysqli_stmt_fetch($statement)) {
+        mysqli_stmt_free_result($statement);
+        mysqli_stmt_close($statement) or die(mysqli_error($connection));
+        return -1;
+    }
     mysqli_stmt_free_result($statement);
     mysqli_stmt_close($statement) or die(mysqli_error($connection));
 //    require_once('includes/close_connection.php');
     return $n;
 }
 
-function n_competence()
+function n_competence($username)
 {
-    global $connection, $username;
-    $query = 'SELECT COUNT(*) FROM competenze_esperti WHERE esperto = ?';
+    global $connection;
+    $n = 0;
+    $query = 'SELECT COUNT(*) AS n FROM competenze_esperti WHERE esperto = ?';
     $statement = mysqli_prepare($connection, $query) or die(mysqli_error($connection));
     mysqli_stmt_bind_param($statement, 's', $username) or die(mysqli_error($connection));
     mysqli_stmt_execute($statement) or die(mysqli_error($connection));
     mysqli_stmt_bind_result($statement, $n) or die(mysqli_error($connection));
+    if (!mysqli_stmt_fetch($statement)) {
+        mysqli_stmt_free_result($statement);
+        mysqli_stmt_close($statement) or die(mysqli_error($connection));
+        return -1;
+    }
     mysqli_stmt_free_result($statement);
     mysqli_stmt_close($statement) or die(mysqli_error($connection));
 //    require_once('includes/close_connection.php');
     return $n;
 }
 
-function n_accepted()
+function n_accepted_from_expert($username)
 {
-    global $connection, $username;
-    $query = 'SELECT COUNT(*) FROM disponibilita WHERE esperto = ? AND data_assegnazione IS NOT NULL';
+    global $connection;
+    $n = 0;
+    $query = 'SELECT COUNT(*) AS n FROM disponibilita WHERE esperto = ? AND data_assegnazione IS NOT NULL';
     $statement = mysqli_prepare($connection, $query) or die(mysqli_error($connection));
     mysqli_stmt_bind_param($statement, 's', $username) or die(mysqli_error($connection));
     mysqli_stmt_execute($statement) or die(mysqli_error($connection));
     mysqli_stmt_bind_result($statement, $n) or die(mysqli_error($connection));
+    if (!mysqli_stmt_fetch($statement)) {
+        mysqli_stmt_free_result($statement);
+        mysqli_stmt_close($statement) or die(mysqli_error($connection));
+        return -1;
+    }
     mysqli_stmt_free_result($statement);
     mysqli_stmt_close($statement) or die(mysqli_error($connection));
 //    require_once('includes/close_connection.php');
     return $n;
 }
 
-function n_rejected()
+function n_rejected_from_expert($username)
 {
-    global $connection, $username;
-    $query = 'SELECT COUNT(*) FROM disponibilita WHERE esperto = ? AND data_rifiuto IS NOT NULL';
+    global $connection;
+    $n = 0;
+    $query = 'SELECT COUNT(*) AS n FROM disponibilita WHERE esperto = ? AND data_rifiuto IS NOT NULL';
     $statement = mysqli_prepare($connection, $query) or die(mysqli_error($connection));
     mysqli_stmt_bind_param($statement, 's', $username) or die(mysqli_error($connection));
     mysqli_stmt_execute($statement) or die(mysqli_error($connection));
     mysqli_stmt_bind_result($statement, $n) or die(mysqli_error($connection));
+    if (!mysqli_stmt_fetch($statement)) {
+        mysqli_stmt_free_result($statement);
+        mysqli_stmt_close($statement) or die(mysqli_error($connection));
+        return -1;
+    }
+    mysqli_stmt_close($statement) or die(mysqli_error($connection));
+//    require_once('includes/close_connection.php');
+    return $n;
+}
+
+function n_dangling_from_expert($username)
+{
+    global $connection;
+    $n = 0;
+    $query = 'SELECT COUNT(*) AS n FROM disponibilita WHERE esperto = ? AND data_assegnazione IS NULL AND data_rifiuto IS NULL';
+    $statement = mysqli_prepare($connection, $query) or die(mysqli_error($connection));
+    mysqli_stmt_bind_param($statement, 's', $username) or die(mysqli_error($connection));
+    mysqli_stmt_execute($statement) or die(mysqli_error($connection));
+    mysqli_stmt_bind_result($statement, $n) or die(mysqli_error($connection));
+    if (!mysqli_stmt_fetch($statement)) {
+        mysqli_stmt_free_result($statement);
+        mysqli_stmt_close($statement) or die(mysqli_error($connection));
+        return -1;
+    }
     mysqli_stmt_free_result($statement);
     mysqli_stmt_close($statement) or die(mysqli_error($connection));
 //    require_once('includes/close_connection.php');
     return $n;
 }
 
-function n_dangling()
+function n_availability_from_expert($username)
 {
-    global $connection, $username;
-    $query = 'SELECT COUNT(*) FROM disponibilita WHERE esperto = ? AND data_assegnazione IS NULL AND data_rifiuto IS NULL';
+    global $connection;
+    $n = 0;
+    $query = 'SELECT COUNT(*) AS n FROM disponibilita WHERE esperto = ?';
     $statement = mysqli_prepare($connection, $query) or die(mysqli_error($connection));
     mysqli_stmt_bind_param($statement, 's', $username) or die(mysqli_error($connection));
     mysqli_stmt_execute($statement) or die(mysqli_error($connection));
     mysqli_stmt_bind_result($statement, $n) or die(mysqli_error($connection));
-    mysqli_stmt_free_result($statement);
-    mysqli_stmt_close($statement) or die(mysqli_error($connection));
-//    require_once('includes/close_connection.php');
-    return $n;
-}
-
-function n_availability()
-{
-    global $connection, $username;
-    $query = 'SELECT COUNT(*) FROM disponibilita WHERE esperto = ?';
-    $statement = mysqli_prepare($connection, $query) or die(mysqli_error($connection));
-    mysqli_stmt_bind_param($statement, 's', $username) or die(mysqli_error($connection));
-    mysqli_stmt_execute($statement) or die(mysqli_error($connection));
-    mysqli_stmt_bind_result($statement, $n) or die(mysqli_error($connection));
+    if (!mysqli_stmt_fetch($statement)) {
+        mysqli_stmt_free_result($statement);
+        mysqli_stmt_close($statement) or die(mysqli_error($connection));
+        return -1;
+    }
     mysqli_stmt_free_result($statement);
     mysqli_stmt_close($statement) or die(mysqli_error($connection));
 //    require_once('includes/close_connection.php');
