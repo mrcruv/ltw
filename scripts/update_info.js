7840b2c39244f6638f78dde08b638d10954d2c97
deto235 <102602333+deto235@users.noreply.github.com>
2022-05-24 18:11:07 +0200

    Create update_info.js
    creazione script update info

---
+++ scripts/update_info.js
@@ -0,0 +1,30 @@
+var current_value;
+
+$(document).ready(function () {
+
+    $('.edit').click(function () {
+        var id = $(this).attr('id');
+        var array = id.split("_");
+        current_value = $('#text_' + array[0]).val();
+        $('#text_' + array[0]).prop('disabled', false);
+        $('#text_' + array[0]).removeClass("hiddenborder");
+        $(this).parents("li").find(".edit, .save, .cancel").toggle();
+    });
+
+    $('.save').click(function () {
+        var id = $(this).attr('id');
+        var array = id.split("_");
+        $('#text_' + array[0]).prop('disabled', true);
+        $('#text_' + array[0]).addClass("hiddenborder");
+        $(this).parents("li").find(".edit, .save, .cancel").toggle();
+    });
+
+    $('.cancel').click(function () {
+        var id = $(this).attr('id');
+        var array = id.split("_");
+        $('#text_' + array[0]).val(current_value);
+        $('#text_' + array[0]).prop('disabled', true);
+        $('#text_' + array[0]).addClass("hiddenborder");
+        $(this).parents("li").find(".edit, .cancel, .save").toggle();
+    });
+});
