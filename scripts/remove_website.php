a8c67bd75436b92b0095a24d720a0916178478ac
Marco Ruvolo <marco.ruvolo@protonmail.com>
2022-05-25 16:26:17 +0200

    Update remove_website.php
    aggiunto controllo sito web

--- scripts/remove_website.php
+++ scripts/remove_website.php
@@ -12,10 +12,13 @@ mysqli_stmt_bind_param($statement, 's', $username) or die(mysqli_error($connecti
 mysqli_stmt_execute($statement) or die(mysqli_error($connection));
 mysqli_stmt_bind_result($statement, $old_website) or die(mysqli_error($connection));
 if (!mysqli_stmt_fetch($statement)) {
-    mysqli_free_result($statement);
     mysqli_stmt_close($statement) or die(mysqli_error($connection));
     header('Location: ../me.php?err=utente+non+esistente');
     die('utente non esistente');
+} else if ($old_website == NULL) {
+    mysqli_stmt_close($statement) or die(mysqli_error($connection));
+    header('Location: ../me.php?err=sito+web+non+esistente');
+    die('sito web non esistente');
 } else {
     mysqli_stmt_close($statement) or die(mysqli_error($connection));
     $query = 'UPDATE utenti SET sito_web = NULL WHERE username = ?';
