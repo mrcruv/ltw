3be1f6d78e5c8ec72a313adb9b4ff3cca848ba40
Marco Ruvolo <marco.ruvolo@protonmail.com>
2022-05-28 17:47:15 +0200

    Update logout.php
    fix minori (interazione con update_password.php)

--- scripts/logout.php
+++ scripts/logout.php
@@ -1,6 +1,8 @@
 <?php
+$msg = isset($_GET['msg']) ? trim($_GET['msg']) : false;
 session_start();
 session_unset();
 session_destroy();
-header('Location: ../index.php?msg=logout+effettuato+con+successo');
+if (!empty($msg)) header('Location: ../index.php?msg=' . str_replace('%20', '+', $msg));
+else header('Location: ../index.php?msg=logout+effettuato+con+successo');
 
