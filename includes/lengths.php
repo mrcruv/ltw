4d58595c202dfefba2b71154350701d2f017d3f8
Marco Ruvolo <marco.ruvolo@protonmail.com>
2022-05-28 16:23:06 +0200

    Create lengths.php
    creazione file con lunghezze max e min

---
+++ includes/lengths.php
@@ -0,0 +1,27 @@
+<?php
+$username_maxlength = 40;
+$cf_maxlength = 16;
+$pec_maxlength = 255;
+$piva_maxlength = 11;
+$website_maxlength = 255;
+
+$entity_name_maxlength = 50;
+
+$expert_name_maxlength = 255;
+$expert_surname_maxlength = $expert_name_maxlength;
+$expert_city_maxlength = $expert_name_maxlength;
+
+$password_maxlength = 24;
+$password_minlength = 8;
+
+$title_name_maxlength = 255;
+$title_notes_maxlength = 255;
+$title_grade_maxlength = 3;
+
+$process_name_maxlength = 255;
+$process_type_maxlength = 255;
+$process_description_maxlength = 255;
+
+$competence_name_maxlength = 255;
+$competence_area_maxlength = 255;
+$competence_description_maxlength = 255;
