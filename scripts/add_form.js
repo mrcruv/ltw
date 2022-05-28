90441776ac6eb4d0922b3e6e38e0da45fc3fea47
deto235 <102602333+deto235@users.noreply.github.com>
2022-05-28 18:01:39 +0200

    Update add_form.js
    fix grafico

--- scripts/add_form.js
+++ scripts/add_form.js
@@ -3,8 +3,10 @@ $(document).ready(function () {
     $("#add_button").click(function () {
         if ($(".add_form").is(":visible")) {
             $(".add_form").hide();
+            $("#add_button").css("filter","brightness(1)");
         } else {
             $(".add_form").show();
+            $("#add_button").css("filter","brightness(0.75)");
         }
     });
 });
