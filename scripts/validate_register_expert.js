46609cef79bb838ab1eac55bd4a8307de31e83ff
deto235 <102602333+deto235@users.noreply.github.com>
2022-05-30 11:04:07 +0200

    Update validate_register_expert.js
    fix pec e website

--- scripts/validate_register_expert.js
+++ scripts/validate_register_expert.js
@@ -1,6 +1,6 @@
 const expert_username_regex = /^[a-zA-Z0-9_]{1,30}$/;
 const expert_cf_regex = /[A-Za-z]{6}[0-9lmnpqrstuvLMNPQRSTUV]{2}[abcdehlmprstABCDEHLMPRST]{1}[0-9lmnpqrstuvLMNPQRSTUV]{2}[A-Za-z]{1}[0-9lmnpqrstuvLMNPQRSTUV]{3}[A-Za-z]{1}/;
-const expert_pec_regex = /(?:\w*.?pec(?:.?\w+)*)/;
+const expert_pec_regex = /[A-z0-9\.\+_-]+@[pec]+\.[a-z]{2,3}/;
 const expert_piva_regex = /^[0-9]{11}$/;
 const expert_website_regex = /^((https?|ftp|smtp):\/\/)(www.)[a-z0-9]+\.[a-z]+(\/[a-zA-Z0-9#]+\/?)*$/;
 const expert_name_regex = /^[a-zA-Z ]{1,255}$/;
@@ -32,7 +32,10 @@ jQuery.validator.addMethod("expert_piva_regex", function(value, element) {
     return expert_piva_regex.test(value);   
     });
 jQuery.validator.addMethod("expert_website_regex", function(value, element) {
-    return expert_website_regex.test(value);   
+    if(value == '')
+        return true;
+    else
+        return expert_website_regex.test(value);   
     });
 jQuery.validator.addMethod("expert_password_regex", function(value, element) {
     return expert_password_regex.test(value);
