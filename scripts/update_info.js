ea1c75d55fe53a3f1a023c70c7414e8be6972269
deto235 <102602333+deto235@users.noreply.github.com>
2022-05-30 11:51:37 +0200

    Update update_info.js
    fix errore update

--- scripts/update_info.js
+++ scripts/update_info.js
@@ -1,4 +1,9 @@
-var current_value;
+var current_cf;
+var current_piva;
+var current_website;
+var current_pec;
+var current_entityName;
+var current_entityType;
 
 $(document).ready(function () {
 
@@ -11,7 +16,24 @@ $(document).ready(function () {
     $('.edit').click(function () {
         var id = $(this).attr('id');
         var array = id.split("_");
-        current_value = $('#text_' + array[0]).val();
+        if (array[0] == 'cf') {
+            current_cf = $('#text_' + array[0]).val();
+        }
+        if (array[0] == 'piva') {
+            current_piva = $('#text_' + array[0]).val();
+        }
+        if (array[0] == 'website') {
+            current_website = $('#text_' + array[0]).val();
+        }
+        if (array[0] == 'pec') {
+            current_pec = $('#text_' + array[0]).val();
+        }
+        if (array[0] == 'entityName') {
+            current_entityName = $('#text_' + array[0]).val();
+        }
+        if (array[0] == 'entityType') {
+            current_entityType = $('#text_' + array[0]).val();
+        }
         $('#text_' + array[0]).prop('disabled', false);
         if ($('#text_' + array[0]).val() == 'N.D.') {
             $('#text_' + array[0]).val('');
@@ -38,7 +60,24 @@ $(document).ready(function () {
 
         $(this).parents("li").find(".update").validate().resetForm();
 
-        $('#text_' + array[0]).val(current_value);
+        if (array[0] == 'cf') {
+            $('#text_' + array[0]).val(current_cf);
+        }
+        if (array[0] == 'piva') {
+            $('#text_' + array[0]).val(current_piva);
+        }
+        if (array[0] == 'website') {
+            $('#text_' + array[0]).val(current_website);
+        }
+        if (array[0] == 'pec') {
+            $('#text_' + array[0]).val(current_pec);
+        }
+        if (array[0] == 'entityName') {
+            $('#text_' + array[0]).val(current_entityName);
+        }
+        if (array[0] == 'entityType') {
+            $('#text_' + array[0]).val(current_entityType);
+        }
         $('#text_' + array[0]).prop('disabled', true);
         $('#text_' + array[0]).addClass("hiddenborder");
         $(this).parents("li").find(".edit, .cancel, .save").toggle();
