86ac0c617b7ba5838b4c5d8278e5455df94ed45f
Marco Ruvolo <marco.ruvolo@protonmail.com>
2022-05-30 11:12:06 +0200

    Update validate_register_entity.js
    pulizia codice

--- scripts/validate_register_entity.js
+++ scripts/validate_register_entity.js
@@ -1,6 +1,6 @@
 const entity_username_regex = /^[a-zA-Z0-9_]{1,30}$/;
 const entity_cf_regex = /[A-Za-z]{6}[0-9lmnpqrstuvLMNPQRSTUV]{2}[abcdehlmprstABCDEHLMPRST]{1}[0-9lmnpqrstuvLMNPQRSTUV]{2}[A-Za-z]{1}[0-9lmnpqrstuvLMNPQRSTUV]{3}[A-Za-z]{1}/;
-const entity_pec_regex = /[A-z0-9\.\+_-]+@[pec]+\.[a-z]{2,3}/;
+const entity_pec_regex = /[A-z0-9.+_-]+@[pec]+\.[a-z]{2,3}/;
 const entity_piva_regex = /^[0-9]{11}$/;
 const entity_website_regex = /^((https?|ftp|smtp):\/\/)(www.)[a-z0-9]+\.[a-z]+(\/[a-zA-Z0-9#]+\/?)*$/;
 const entity_name_regex = /^[a-zA-Z0-9.-_ ]{1,50}$/;
